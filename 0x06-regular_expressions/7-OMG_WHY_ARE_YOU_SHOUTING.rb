3381de033e36d752c90ad7d365a0b91e7aec92b6
Arafa Khalaf <khalafarafa@gmail.com>
2024-02-28 11:24:50 -0800

    0x06 - 12

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+#  regular expression match `School`
+
+# Check if an argument is provided
+if ARGV.empty?
+  exit 1
+end
+
+# Get the argument
+input_string = ARGV[0]
+
+# Regular expression pattern
+pattern = /[A-Z]/
+
+# Find all occurrences of the pattern in the input string
+matches = input_string.scan(pattern)
+
+# Check if the input string matches the pattern
+if input_string.match?(pattern)
+  matches.each do |match|
+    print match
+  end
+end
