a69670ae1161678a6d72925e335085fbea4d7901
Arafa Khalaf <khalafarafa@gmail.com>
2024-02-28 06:28:30 -0800

    0x06 - 03

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,21 @@
 #!/usr/bin/env ruby
 #  regular expression match `School`
 
-Scho*l
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# Get the argument
+input_string = ARGV[0]
+
+# Regular expression pattern
+pattern = /Scho*l/
+
+# Check if the input string matches the pattern
+if input_string.match?(pattern)
+  puts "The input string matches the pattern."
+else
+  puts "The input string does not match the pattern."
+end
